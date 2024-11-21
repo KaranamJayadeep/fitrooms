5a28620d3d9e062745ffae8f99943cfa8addb0e4
KaranamJayadeep <153343866+KaranamJayadeep@users.noreply.github.com>
2024-11-21 23:51:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let imgspace=document.getElementById("imgspace");
+let para2=document.getElementById("para2");
+function medico(){
+    imgspace.style.backgroundImage="url('https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+    para2.innerHTML='A doctor is a medical expert who helps diagnose, treat, and prevent health problems. Consulting a doctor ensures proper care and guidance for maintaining your well-being. Whether for routine check-ups or specific concerns, they provide advice and treatment tailored to your needs. <a href="https://docjd.vercel.app/" target="_blank">Click here to contact our doctor</a>';
+}
+function gym(){
+    imgspace.style.backgroundImage="url('https://images.unsplash.com/photo-1546749876-2088f8b19e09?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+    para2.innerHTML="A gym is a place equipped with facilities for exercise and fitness activities. Visiting a gym helps improve physical health and overall well-being. Whether for strength training or general fitness, gyms provide equipment and guidance tailored to your fitness goals.";
+}
+function swim(){
+    imgspace.style.backgroundImage="url('https://images.unsplash.com/photo-1536331568701-0b15cbb1a918?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+    para2.innerHTML="Swimming is an enjoyable activity that enhances fitness and overall well-being. Regular swimming improves strength, flexibility, and cardiovascular health while reducing stress. Whether for leisure or exercise, it offers numerous benefits suited to your goals.";
+}
+function trans(){
+    imgspace.style.backgroundImage="url('https://images.unsplash.com/photo-1528578976328-4bc1ea9fad47?q=80&w=1836&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+    para2.innerHTML="We offer convenient transport services to make your stay more comfortable. Whether you're traveling to your room from the airport or exploring nearby attractions, our facility ensures smooth and hassle-free commutes. Experience the ease of reliable transportation designed with your convenience in mind.";
+}
